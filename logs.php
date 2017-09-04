381f8d0245f46062134baf165d2f5d3081c253f1
php livre <sac.phplivre@gmail.com>
2017-09-04 14:38:03 -0300

    Create logs.php

---
+++ logs.php
@@ -0,0 +1,14 @@
+function logs($x1) {
+
+// endereço url
+$end = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER ['REQUEST_URI'];
+
+// sessão ou cookie do nome do usuário
+if(empty($_SESSION[usuario])) { $eml = "Deslogado"; } else { $eml = $_SESSION[usuario]; }
+
+// dados de conexão do servidor mysql
+$mysqli = new mysqli ("localhost","root","12345678","db");
+
+// query para inserir
+$mysqli -> query ("insert into ".$cfg_sv[prefixo]."logs (log,data,email,ip,pagina) values ('".$x1."','".date('Y-m-d H:i:s')."','".$eml."','".$_SERVER[REMOTE_ADDR]."','".$end."')");
+}
